441c09054097f91ff4d28ca3bdd08cdb5511368c
Evan Zalys <gaze@bea.ms>
2013-10-02 15:46:37 -0400

    Python stuff

--- init-python.el
+++ init-python.el
@@ -1,3 +1,12 @@
 (z-require-package 'elpy)
 
+(elpy-enable)
+(elpy-use-ipython)
+
+(defun annotate-pdb ()
+  (interactive)
+  (highlight-lines-matching-regexp "import ipdb")
+  (highlight-lines-matching-regexp "ipdb.set_trace()"))
+(add-hook 'python-mode-hook 'annotate-pdb)
+
 (provide 'init-python)
