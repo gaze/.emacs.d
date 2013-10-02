025464ff44b23298de9f5732ec8372df38dd8963
Evan Zalys-Geller <gaze@bea.ms>
2013-10-02 10:01:40 -0400

    Working

--- init.el
+++ init.el
@@ -1,15 +1,28 @@
-(require 'setup-package)
-
+(require 'org)
 (add-to-list 'load-path user-emacs-directory)
-(add-to-list 'load-path site-lisp-dir)
 
-(defun init--install-packages ()
-  (packages-install
-   '(magit
-     paredit)))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-(condition-case nil
-    (init--install-packages)
-  (error
-   (package-refresh-contents)
-   (init--install-packages)))
+(package-initialize)
+
+(defun z-require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (z-require-package package min-version t)))))
+
+
+(z-require-package 'magit)
+
+
+;; (require 'init-auto-complete)
+(require 'init-python)
+(require 'init-ui)
