b042ed08c7a313a55b463a71c7d8c02572b6645d
Evan <ezalys@mit.edu>
2021-11-18 00:25:09 -0500

    org mode

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))))
+    ("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -35,102 +35,18 @@
 (straight-use-package 'use-package)
 (require 'use-package)
 
-(straight-use-package 'evil)
-
-(use-package evil
-                      :init
-                      (setq evil-want-keybinding nil)
-                      (evil-mode 1))
-
 (straight-use-package 'exec-path-from-shell)
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
 
-;; (require 'haskell-mode)
 (require 'init-ui)
-;; (require 'init-evil)
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "settings.org"
+                   user-emacs-directory))
+
+;; (require 'haskell-mode)
 ;; (require 'init-vhdl)
 ;; (require 'init-python)
-; (require 'yasnippet)
-
-(straight-use-package 'clojure-mode)
-(straight-use-package 'cider)
-(straight-use-package 'smartparens)
-(add-hook 'clojure-mode-hook #'smartparens-strict-mode)
-
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-
-;; highlight parens
-(require 'paren)
-(show-paren-mode t)
-
-
-;; C++ Features
-
-(straight-use-package 'lsp-mode)
-(straight-use-package 'lsp-ui)
-(straight-use-package 'company-lsp)
-(straight-use-package 'ccls)
-
-(use-package lsp-mode :commands lsp)
-(use-package lsp-ui :commands lsp-ui-mode)
-
-(use-package company
-  :init
-  (setq completion-styles `(basic partial-completion emacs22 initials
-                                ,(if (version<= emacs-version "27.0") 'helm-flex 'flex)))
-  (setq company-tooltip-align-annotations t
-        company-tooltip-limit 14
-        company-idle-delay 0
-        company-echo-delay (if (display-graphic-p) nil 0)
-        company-minimum-prefix-length 2
-        company-require-match 'never
-        company-dabbrev-ignore-case nil
-        company-dabbrev-downcase nil
-        company-global-modes '(not erc-mode message-mode help-mode gud-mode eshell-mode shell-mode)
-        company-backends '(company-capf)
-        company-frontends '(company-pseudo-tooltip-frontend
-                            company-echo-metadata-frontend))
-  :config
-  (global-company-mode +1))
-
-(use-package company-lsp :commands company-lsp)
-(setq company-tooltip-align-annotations t)
-
-(use-package ccls
-             :hook ((cmode c++mode objc-mode cuda-mode) .
-                    (lambda () (require 'ccls) (lsp))))
-
-(setq ccls-sem-highlight-method 'font-lock)
-
-(setq ccls-executable "/usr/bin/ccls")
-
-(straight-use-package 'ivy)
-(straight-use-package 'counsel)
-
-(use-package ivy :ensure t
-  :diminish (ivy-mode . "")
-  :bind
-  (:map ivy-mode-map
-   ("C-'" . ivy-avy))
-  :config
-  (ivy-mode 1)
-  ;; add ‘recentf-mode’ and bookmarks to ‘ivy-switch-buffer’.
-  (setq ivy-use-virtual-buffers t)
-  ;; number of result lines to display
-  (setq ivy-height 10)
-  ;; does not count candidates
-  (setq ivy-count-format "")
-  ;; no regexp by default
-  (setq ivy-initial-inputs-alist nil)
-  ;; configure regexp engine.
-  (setq ivy-re-builders-alist
-	;; allow input not in order
-        '((t   . ivy--regex-ignore-order))))
-
-
-(straight-use-package 'projectile)
-(projectile-mode +1)
-
-(straight-use-package 'magit)
+;; (require 'yasnippet)
--- modules/init-ui.el
+++ modules/init-ui.el
@@ -1,91 +1,27 @@
-;; WINDOW
-
-(straight-use-package 'nord-theme)
-(load-theme 'nord t)
-
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
-
-(menu-bar-mode -1)
-
-;; the blinking cursor is nothing, but an annoyance
-(blink-cursor-mode -1)
-
-;; disable startup screen
-(setq inhibit-startup-screen t)
-
-;; nice scrolling
-;; (setq scroll-margin 0
-;;       scroll-conservatively 100000
-;;       scroll-preserve-screen-position 1)
-
-;;; GUTTER / FRINGE
-
-;; make the fringe (gutter) smaller
-;; the argument is a width in pixels (the default is 8)
-(if (fboundp 'fringe-mode)
-    (fringe-mode 4))
-
-(straight-use-package 'git-gutter-fringe)
-(when (window-system)
-  (require 'git-gutter-fringe))
-
-(global-git-gutter-mode +1)
-(setq-default indicate-buffer-boundaries 'left)
-(setq-default indicate-empty-lines +1)
-
-;;; MODELINE
-
-(line-number-mode t)
-(column-number-mode t)
-(size-indication-mode t)
-
-(straight-use-package 'smart-mode-line)
-(sml/setup)
-(sml/apply-theme 'respectful)
-
-;; INPUT BEHAVIOR
 
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-(setq redisplay-dont-pause t
-      scroll-margin 1
-      scroll-step 1
-      scroll-conservatively 10000
-      scroll-preserve-screen-position 1)
-
-(setq mouse-wheel-follow-mouse 't)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-
-;; enable y/n answers
-(fset 'yes-or-no-p 'y-or-n-p)
 
 ;; BUFFER
 
-(straight-use-package 'rainbow-delimiters)
-(require 'rainbow-delimiters)
-
-(set-face-attribute 'default nil :font "Source Code Pro 12")
-(set-face-attribute 'fixed-pitch nil :font "Source Code Pro 12")
 ;;(set-face-attribute 'variable-pitch nil :font "Pt Sans-12")
 ;;(set-face-attribute 'font-lock-comment-face nil :font "Pt Sans-12")
 
-(straight-use-package 'fill-column-indicator)
-(define-globalized-minor-mode global-fci-mode fci-mode
-  (lambda () (fci-mode 1)))
+;;(straight-use-package 'fill-column-indicator)
+;;(define-globalized-minor-mode global-fci-mode fci-mode
+;;  (lambda () (fci-mode 1)))
 
-(global-fci-mode 1)
+;;(global-fci-mode 1)
 
-(font-lock-add-keywords nil
-    '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))
-
+;;(font-lock-add-keywords nil
+;;    '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))
 
 ;;;;;;;;;;; BETTER DEFAULTS from technomancy
 
-(ido-mode t)
-(setq ido-enable-flex-matching t)
+;;(ido-mode t)
+;;(setq ido-enable-flex-matching t)
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
@@ -101,15 +37,5 @@
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
-(show-paren-mode 1)
-(setq-default indent-tabs-mode nil)
-(setq x-select-enable-clipboard t
-    x-select-enable-primary t
-    save-interprogram-paste-before-kill t
-    apropos-do-all t
-    mouse-yank-at-point t
-    save-place-file (concat user-emacs-directory "places")
-    backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                             "backups"))))
 
 (provide 'init-ui)
